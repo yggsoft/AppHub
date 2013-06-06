4adfee273adf7871ed946b1f440eb1d5f443097a
yggsoft <yggsoft@163.com>
2013-06-06 21:40:20 +0800

    ---

--- LoggerAnalysis/init.bat
+++
@@ -1,7 +0,0 @@
-rem mvn archetype:create -DgroupID=com.angelo -DartifactId=LoggerAnalysis
-
-vi .gitignore
-
-git add .
-git commit -m "--"
-git push
--- LoggerAnalysis/src/main/java/com/angelo/logging/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.angelo.logging;
-
-public class Constants {
-	public static final String LINE_SEPRATOR = System.getProperty("line.separator");
-}
